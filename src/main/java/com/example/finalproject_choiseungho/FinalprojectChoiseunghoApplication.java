c5287ea4ae68b5ebb6a477b66e04317b92fa86bb
gemdoq <gemdoq@gmail.com>
2022-12-20 11:29:25 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/finalproject_choiseungho/FinalprojectChoiseunghoApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalproject_choiseungho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalprojectChoiseunghoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalprojectChoiseunghoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/finalproject_choiseungho/FinalprojectChoiseunghoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalproject_choiseungho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectChoiseunghoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
