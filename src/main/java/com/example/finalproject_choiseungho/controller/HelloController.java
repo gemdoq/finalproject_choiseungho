7ed14b9193a56eba2f7405daf19ac92bfe12c987
gemdoq <gemdoq@gmail.com>
2022-12-27 09:09:04 +0900

    doc: happy_new_year -> 최승호

--- src/main/java/com/example/finalproject_choiseungho/controller/HelloController.java
+++ src/main/java/com/example/finalproject_choiseungho/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/api/v1/hello")
     public String hello() {
-        return "happy_new_year";
+        return "최승호";
     }
 }
