e7ba7075d4bca7ecfbf18ad31ed54eac0c0a077b
gemdoq <gemdoq@gmail.com>
2022-12-29 10:12:13 +0900

    feature: sumOfDigits 기능 추가

--- src/main/java/com/example/finalproject_choiseungho/controller/HelloController.java
+++ src/main/java/com/example/finalproject_choiseungho/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.finalproject_choiseungho.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,17 @@ public class HelloController {
     @GetMapping("/api/v1/hello")
     public String hello() {
         return "최승호";
+    }
+
+    @GetMapping("/api/v1/hello/{num}")
+    public int sumOfDigits(@PathVariable Integer num) {
+        int answer = 0;
+
+        while(num!=0){
+            answer += num%10;
+            num /= 10;
+        }
+
+        return answer;
     }
 }
