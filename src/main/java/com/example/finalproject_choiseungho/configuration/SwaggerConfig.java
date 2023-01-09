8c108c33ecaf36c5c57cfbfb11b6389125e97cfd
gemdoq <gemdoq@gmail.com>
2023-01-09 09:45:50 +0900

    doc: SwaggerConfig 수정

--- src/main/java/com/example/finalproject_choiseungho/configuration/SwaggerConfig.java
+++ src/main/java/com/example/finalproject_choiseungho/configuration/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.example.finalproject_choiseungho.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -19,6 +21,7 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
+                .ignoredParameterTypes(Authentication.class, Pageable.class)
                 .securityContexts(Arrays.asList(securityContext()))
                 .securitySchemes(Arrays.asList(apiKey()))
                 .apiInfo(apiInfo())
