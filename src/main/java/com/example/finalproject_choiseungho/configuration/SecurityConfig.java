ba2bc44db107b56a1542c278191a5146899d6c1a
gemdoq <gemdoq@gmail.com>
2023-01-09 09:52:38 +0900

    doc: SecurityConfig.java 수정

--- src/main/java/com/example/finalproject_choiseungho/configuration/SecurityConfig.java
+++ src/main/java/com/example/finalproject_choiseungho/configuration/SecurityConfig.java
@@ -22,21 +22,27 @@ public class SecurityConfig {
     @Value("${jwt.secret}")
     private String secretKey;
 
+    private final String[] SWAGGER = {
+            "/v3/api-docs",
+            "/swagger-resources/**", "/configuration/security", "/webjars/**",
+            "/swagger-ui.html", "/swagger/**", "/swagger-ui/**"};
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .httpBasic().disable()
                 .csrf().disable()
                 .cors().and()
-                .authorizeRequests()
-                .antMatchers("/swagger-ui/").permitAll()
-                .antMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll() // join, login은 언제나 가능
-                .antMatchers(HttpMethod.POST, "/api/v1/posts").authenticated()
-                .antMatchers(HttpMethod.PUT, "/api/v1/posts").authenticated()
-                .antMatchers(HttpMethod.DELETE, "/api/v1/posts").authenticated()
-                .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt사용하는 경우 씀
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/v1/hello/**").permitAll()
+                .antMatchers(SWAGGER).permitAll()
+                .antMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll() // join, login은 언제나 가능
+                .antMatchers("/api/v1/posts/my").authenticated()
+                .antMatchers(HttpMethod.GET, "/api/v1/posts/**").permitAll()
+                .antMatchers("/api/**").authenticated()
                 .and()
                 .addFilterBefore(new JwtFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class)
                 .build();
